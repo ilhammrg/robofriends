f8c03a6c487f5fe44b7588a8a536c98e907bea9c
Ilham Muhammad <ilhammrg@gmail.com>
2020-05-06 17:30:01 +0700

    Updates

---
+++ precache-manifest.fa7fc3b9e3585443f9516bf836979cdc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40246ed74f7b60d8278ece9698e877ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84ca485f6fb5f1978a9f",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "298444dfe3601179b772",
+    "url": "/static/css/main.4bf8a5e2.chunk.css"
+  },
+  {
+    "revision": "84ca485f6fb5f1978a9f",
+    "url": "/static/js/2.12e19136.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.12e19136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "298444dfe3601179b772",
+    "url": "/static/js/main.28e64bec.chunk.js"
+  },
+  {
+    "revision": "df16bcf85c25d69dad33",
+    "url": "/static/js/runtime-main.004a6136.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
